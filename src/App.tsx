29a332a2337c11ccd4baac0ebbf675fdeef40ddd
wrekcaparamarthika <sttwrekcaparamarthika@gmail.com>
2024-07-04 16:15:09 +0800

    use useQuery in App components

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,37 @@ import GlobalDebitKredit from './sections/GlobalDebitKredit';
 import SourceIncome from './sections/SourceIncome';
 import GlobalTiketing from './sections/GlobalTiketing';
 import StatusSponsorship from './sections/StatusSponsorship';
+import { useQuery } from '@tanstack/react-query';
+import { request } from './lib/utils';
 
 const App = () => {
-	const [dataOverview, setDataOverview] = React.useState<DataOverview>(
-		DEFAULT_VALUE_DATA_OVERVIEW
-	);
-	const [dataSponsor, setDataSponsor] = React.useState<Sponsor>(
-		DEFAULT_VALUE_LIST_SPONSOR
-	);
-	const [isLoading, setLoading] = React.useState<boolean>(false);
-	React.useEffect(() => {
-		async function init() {
-			const overview = await fetchData(API_OVERVIEW);
-			const sponsor = await fetchData(API_SPONSOR);
-			if (overview) setDataOverview(overview);
-			if (sponsor) setDataSponsor(sponsor);
-			if (overview && sponsor) setLoading(true);
-		}
-		init();
-	}, []);
+	// const [dataOverview, setDataOverview] = React.useState<DataOverview>(
+	// 	DEFAULT_VALUE_DATA_OVERVIEW
+	// );
+	// const [dataSponsor, setDataSponsor] = React.useState<Sponsor>(
+	// 	DEFAULT_VALUE_LIST_SPONSOR
+	// );
+	// const [isLoading, setLoading] = React.useState<boolean>(false);
+	// React.useEffect(() => {
+	// 	async function init() {
+	// 		const overview = await fetchData(API_OVERVIEW);
+	// 		const sponsor = await fetchData(API_SPONSOR);
+	// 		if (overview) setDataOverview(overview);
+	// 		if (sponsor) setDataSponsor(sponsor);
+	// 		if (overview && sponsor) setLoading(true);
+	// 	}
+	// 	init();
+	// }, []);
+	const { data: dataOverview, isFetched: fetchedOverview } = useQuery({
+		queryKey: ['overview'],
+		queryFn: () => request<DataOverview>(API_OVERVIEW),
+		initialData: DEFAULT_VALUE_DATA_OVERVIEW,
+	});
+	const { data: dataSponsor, isFetched: fetchedSponsor } = useQuery({
+		queryKey: ['sponsor'],
+		queryFn: () => request<Sponsor>(API_SPONSOR),
+		initialData: DEFAULT_VALUE_LIST_SPONSOR,
+	});
 	const globalSaldo = dataOverview.resultDebitKredit[2]?.total;
 	const globalPercentage =
 		dataOverview.resultDebitKredit[0]?.total /
@@ -41,7 +53,7 @@ const App = () => {
 	return (
 		<main className='flex items-center justify-center w-full px-2.5 py-10 flex-col gap-2.5'>
 			<IFElseComponents
-				state={!isLoading}
+				state={!fetchedOverview && !fetchedSponsor}
 				stateTrue={
 					<h1>Mohon tunggu sebentar data masih di proses...</h1>
 				}
