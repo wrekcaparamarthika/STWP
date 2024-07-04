87d56206c10171feb0fabc556cd2dc18f2c694b6
wrekcaparamarthika <sttwrekcaparamarthika@gmail.com>
2024-07-04 16:09:30 +0800

    QueryclientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { clientQuery } from './lib/utils';
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
+	<QueryClientProvider client={clientQuery}>
+		<ReactQueryDevtools initialIsOpen={true} />
 		<App />
-	</React.StrictMode>
+	</QueryClientProvider>
 );
